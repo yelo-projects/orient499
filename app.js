f706d9f511cf28f1e1b0ae134d281422b7d53816
Xananax <xananax@yelostudio.com>
2013-05-28 01:37:33 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , config = require('konphyg')(__dirname + '/config/')('o499')
+  ,	galleries = require('./lib/galleries')('public/photos',config.galleries)
+;
+config.galleries = galleries;
+
+var app = express();
+app.locals(config)
+
+app.set('port', process.env.PORT || config.port);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser(config.salt));
+app.use(app.router);
+app.use(require('./lib/stylus')(config));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+
+http.createServer(app).listen(config.port, function(){
+  console.log('Express server listening on port ' + config.port);
+});
---
+++ public/js/main.js
@@ -0,0 +1,99 @@
+jQuery(function($){
+
+	$('body').addClass('js');
+
+	var $w = $(window)
+	,	$PagesContainer = $('#Pages')
+	,	$Pages = $('.page').removeClass('active')
+	,	$PagesLinks = $('#Header a')
+	,	$galleriesContainers = $('.gallery')
+	,	$Galleries = $('.gallery-images')
+	,	$Images = $('.gallery-image').removeClass('active')
+	,	$toLoad = $('.gallery-image img')
+	,	elCache = {}
+	,	maxLoad = 3
+	,	currLoad = 0
+	;
+
+	function getEl(el){
+		if(!elCache.hasOwnProperty(el)){
+			elCache[el] = $(el);
+			if(!elCache[el].length){elCache[el] = false;}
+		}
+		return elCache[el];
+	}
+
+	function inView(){
+		$toLoad.not('processed').filter(function(){
+			var $e = $(this)
+			, th = 0
+			, wt = $w.scrollTop()
+			, wb = wt + $w.height()
+			, et = $e.offset().top
+			, eb = et + $e.height()
+			;
+			return eb >= wt - th && et <= wb + th;
+		}).each(function(){
+			lazyLoadImage($(this));
+		})
+	}
+
+	function lazyLoadImage($image,galleryId,imageId,loadInc){
+		if(!loadInc){loadInc=0;}
+		var $imageContainer = $image.parent().parent()
+		if($image.hasClass('processed')){
+			if($image.hasClass('loaded')){
+				$imageContainer.addClass('active');
+			}
+			return;
+		}
+		currLoad+=1-loadInc;
+		$image
+			.on('load',function(){
+				currLoad--;
+				$imageContainer.addClass('active');
+				$image.addClass('loaded').off('load');
+				$toLoad = $toLoad.not($image);
+				if(!galleryId){return;}
+				if(currLoad < maxLoad){
+					var $next = $(galleryId + '-image-' + imageId);
+					if($next.length){lazyLoadImage($next);}
+				}
+			})
+			.addClass('processed')
+			.attr('src',$image.data('src'));
+		return $image;
+	}
+
+	function route(hash){
+		hash = (hash || '#home').split('-image-');
+		var pageId = hash.shift()
+		,	imageId = (hash.shift() || '0')
+		,	page = pageId+'-page'
+		,	image = pageId+'-image-' + imageId
+		,	link = pageId+'-link'
+		,	$page = getEl(page)
+		,	$image = getEl(image)
+		,	$link = getEl(link)
+		;
+		$Images.removeClass('active');
+		lazyLoadImage($image,pageId,imageId);
+		$Pages.not($page.addClass('active')).removeClass('active');
+		$PagesLinks.not($link.addClass('active')).removeClass('active');
+		if(pageId=='#contactus'){
+			lazyLoadImage($('#contactus-page .gallery-image-1 img'),pageId,imageId,1);
+		}
+	}
+
+	$("a[href^=#]").on("click", function(evt){
+		evt.preventDefault();
+		var hash = '#'+this.href.split('#').pop()
+		route(hash);
+		history.pushState({}, "", hash);
+	});
+
+	$w.on('hashchange',function(){route(location.hash);})
+
+	route(location.hash || null);
+
+})
