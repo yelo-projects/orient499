6c69a506c72a96e973547715a2de19208217b91d
Xananax <xananax@yelostudio.com>
2013-07-31 07:34:11 +0300

    corrected path

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ var fs = require('fs');
 exports.index = function(req, res){
   res.render('index',{},function(err,rendered){
   	if(err){return req.next(err);}
-  	fs.writeFile('index.html', rendered, {encoding:'utf8'},function(err){
+  	fs.writeFile('public/index.html', rendered, {encoding:'utf8'},function(err){
   		if(err){throw err;}
   		console.log('index.html generated');
 	});
