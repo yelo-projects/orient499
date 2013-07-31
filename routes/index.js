fff44618e0453d7025754a0135510cf04e6af45e
Xananax <xananax@yelostudio.com>
2013-07-31 07:29:19 +0300

    added index.html generation

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
-
+var fs = require('fs');
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index');
+  res.render('index',{},function(err,rendered){
+  	if(err){return req.next(err);}
+  	fs.writeFile('index.html', rendered, {encoding:'utf8'},function(err){
+  		if(err){throw err;}
+  		console.log('index.html generated');
+	});
+    res.send(rendered);
+  });
 };
